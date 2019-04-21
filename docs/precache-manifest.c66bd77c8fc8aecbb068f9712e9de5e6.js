36df9da155685db71a69c46abc1a039ec296313e
irakli svanidze <irakli1393@gmail.com>
2019-04-21 12:02:19 +0400

    deploy

---
+++ docs/precache-manifest.c66bd77c8fc8aecbb068f9712e9de5e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "290aebd1298ee73ed44d",
+    "url": "/static/js/main.9c2f8812.chunk.js"
+  },
+  {
+    "revision": "150c8bdbc2b2df60b426",
+    "url": "/static/js/2.92d587d9.chunk.js"
+  },
+  {
+    "revision": "290aebd1298ee73ed44d",
+    "url": "/static/css/main.1fe698d8.chunk.css"
+  },
+  {
+    "revision": "150c8bdbc2b2df60b426",
+    "url": "/static/css/2.e93cbef3.chunk.css"
+  },
+  {
+    "revision": "a9aa3289e5d8c56b16b569358be328db",
+    "url": "/index.html"
+  }
+];
